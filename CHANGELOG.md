3a112f3dfd6901ecfe5192499f830114bf5577ca
Zsolt Takács <zsolt@takacs.cc>
2014-02-21 10:50:28 +0100

    release 1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,11 @@
 CHANGELOG
 =========
 
+1.0.1
+-----
+
+ * Fix bug in `Example::sumNumbers()` not adding fourth parameter
+
 1.0.0
 -----
 
--- Example.php
+++ Example.php
@@ -1,5 +1,5 @@
 class Example {
 	public function sumNumbers($sum1, $sum2, $sum3 = 0, $sum4 = 0) {
-		return $sum1 + $sum2 + $sum3;
+		return $sum1 + $sum2 + $sum3 + $sum4;
 	}
 }
