e70f32e00f4934cf334c70836b4278687caf97d0
Zsolt Takács <zsolt@takacs.cc>
2014-02-21 08:58:29 +0100

    release 1.0.0

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+CHANGELOG
+=========
+
+1.0.0
+-----
+
+ * Initial release with `Example::sumNumbers()` supporting up to 4 numbers.
---
+++ Example.php
@@ -0,0 +1,5 @@
+class Example {
+	public function sumNumbers($sum1, $sum2, $sum3 = 0, $sum4 = 0) {
+		return $sum1 + $sum2 + $sum3;
+	}
+}
